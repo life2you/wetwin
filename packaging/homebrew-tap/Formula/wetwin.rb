e34b759beb9282a98349c58fdfd1873f7651e5af
life2you <henry@henglin.com>
2026-05-18 13:40:44 +0800

    chore: refresh wetwin 0.1.3 formula

--- packaging/homebrew-tap/Formula/wetwin.rb
+++ packaging/homebrew-tap/Formula/wetwin.rb
@@ -1,18 +1,18 @@
 class Wetwin < Formula
   desc "Lightweight macOS WeChat multi-instance manager with a terminal UI"
   homepage "https://github.com/life2you/wetwin"
-  version "0.1.1"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/life2you/wetwin/releases/download/v0.1.1/wetwin-aarch64-apple-darwin.tar.gz"
-      sha256 "35a4eb9239a1dfad941c8e4ccf344e406870273a52be30427430846ee91245d7"
+      url "https://github.com/life2you/wetwin/releases/download/v0.1.3/wetwin-aarch64-apple-darwin.tar.gz"
+      sha256 "d1b0616310c10a883c2482df6022ec27787dbd3d0a39494e471852ec35c15e0d"
     end
 
     on_intel do
-      url "https://github.com/life2you/wetwin/releases/download/v0.1.1/wetwin-x86_64-apple-darwin.tar.gz"
-      sha256 "02a31157017a08aa7a4dd305e8395cf7a301074ae711dfbd6897da50125c6af8"
+      url "https://github.com/life2you/wetwin/releases/download/v0.1.3/wetwin-x86_64-apple-darwin.tar.gz"
+      sha256 "2999e4cce979458b355e6c72bfd030bab19b6fb17e41b05dc4e9c713e538d70b"
     end
   end
 
